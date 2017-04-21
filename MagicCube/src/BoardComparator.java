72255d52f6526ecbffac6aaebb3bc545f2a29156
João Castelo <joaocastelo.dev@outlook.com>
2017-04-21 20:12:56 +0100

    Cycle crossover, mutation and other changes
    Bug with board generation. Repeated numbers

--- MagicCube/src/BoardComparator.java
+++ MagicCube/src/BoardComparator.java
@@ -1,9 +1,15 @@
 import java.util.Comparator;
 
 public class BoardComparator implements Comparator<Board> {
-	public int compare(Board b1,Board b2) {
+	
+	/**
+	 * Compara duas boards e ordena-as de acordo com o seu fitness. Menor fitness primeiro
+	 */
+	public int compare(Board b1, Board b2) {
+		
 		int fitness1 = b1.getFitness();
 		int fitness2 = b2.getFitness();
+		
 		if (fitness1 > fitness2) 
 			return 1;
 		else if (fitness1 == fitness2) 
